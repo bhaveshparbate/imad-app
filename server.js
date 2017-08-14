1b9b030401af2cd51a950b029a03720e0de604cb
bhaveshparbate <bhaveshparbate@gmail.com>
2017-08-14 12:49:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,25 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  
 });
+
+app.get('/article-one',function (req,res){
+res.sendfile('article one requested and server here');
+    
+});
+
+app.get('/article-two',function (req,res){
+res.sendfile('article two requested and server here');
+    
+});
+
+app.get('/article-three',function (req,res){
+res.sendfile('article three requested and server here');
+    
+});
+
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
